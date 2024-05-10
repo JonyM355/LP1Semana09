9da0178d3dbb47a490965ea28e120b9615106d8c
JonyM355 <joaoricardoam05@gmail.com>
2024-05-10 23:51:45 +0100

    Os tipos PlayerClass e Player e os métodos GetHashCode() e Equals implementados

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IntCollections
 {
@@ -6,7 +8,75 @@ namespace IntCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> listTest = new List<int>();
+            Stack<int> stackTest = new Stack<int>();
+            Queue<int> queueTest = new Queue<int>();
+            HashSet<int> setTest = new HashSet<int>();
+
+            int[] numberArray = new int[] {1, 10, -30, 10, -5};
+
+            foreach (int n in numberArray)
+            {
+                listTest.Add(n);
+                stackTest.Push(n);
+                queueTest.Enqueue(n);
+                setTest.Add(n);
+            }
+
+            Console.WriteLine(PrintCollection("List",listTest));
+            Console.WriteLine(PrintCollection("Stack",stackTest));
+            Console.WriteLine(PrintCollection("Queue",queueTest));
+            Console.WriteLine(PrintCollection("HashSet",setTest));
+        }
+
+        private static string PrintCollection(string name, IEnumerable<int> col)
+        {
+            string colString = $"{name}: ";
+            int indexCounter = 0;
+
+            foreach (int i in col)
+            {
+                if (indexCounter < col.Count()-1) 
+                    colString += $"{i}, ";
+                else 
+                    colString += $"{i}";
+                
+                indexCounter++;
+            }
+            
+            return colString;
+        }
+
+        private static string PrintCollection(string name, Stack<int> stack)  
+        {
+            string colString = $"{name}: ";
+            int size = stack.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                colString += stack.Pop();
+                
+                if (i < size-1)
+                    colString += ", ";
+            }
+            
+            return colString;
+        }
+        
+        private static string PrintCollection(string name, Queue<int> queue)
+        {
+            string colString = $"{name}: ";
+            int size = queue.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                colString += queue.Dequeue();
+                
+                if (i < size-1)
+                    colString += ", ";
+            }
+            
+            return colString;
         }
     }
 }
