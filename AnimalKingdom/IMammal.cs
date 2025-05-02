73f12d8aa8a9f28e4674627ea25f89a42e96bc01
JonyM355 <joaoricardoam05@gmail.com>
2025-05-02 11:19:27 +0100

    Creating interface IMammal

---
+++ AnimalKingdom/IMammal.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalKingdom
+{
+    public interface IMammal
+    {
+        int NumberOfNipples { get; }
+    }
+}
